1ff3df7ced14d73e2608b06a95106296be72d912
jinhong.brejnholt <jinhong.brejnholt@velux.com>
2023-03-05 22:42:13 +0100

    feat: add cosign kyverno demo.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # demos
+
+A collection of demos.
---
+++ kyverno/cloud_native_aalborg_2023/script.sh
@@ -0,0 +1,2 @@
+k run bad-busy --image=ghcr.io/jbrejnholt/cosign-demo/busybox-unsigned:1.34.1
+k run good-busy --image=ghcr.io/jbrejnholt/cosign-demo/busybox-signed:1.36.0
