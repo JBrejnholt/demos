19b03111fe74bdafbf300b9d1a7ec61db5c38e36
jinhong.brejnholt <jinhong.brejnholt@velux.com>
2023-03-05 22:44:55 +0100

    chore: add script.

--- kyverno/cloud_native_aalborg_2023/script.sh
+++ kyverno/cloud_native_aalborg_2023/script.sh
@@ -1,2 +1,5 @@
+k create ns kyverno
+k apply -f secret-demo.yaml
+helm install -f values.yaml -f values-demo.yaml kyverno -n kyverno .
 k run bad-busy --image=ghcr.io/jbrejnholt/cosign-demo/busybox-unsigned:1.34.1
 k run good-busy --image=ghcr.io/jbrejnholt/cosign-demo/busybox-signed:1.36.0
