5ca0250902314ca4f0acd1b9c0b7edd29bf714ba
jinhong.brejnholt <jinhong.brejnholt@velux.com>
2023-03-27 10:24:48 +0200

    feat: update verify image signature with new syntax; update script.

--- kyverno/cloud_native_aalborg_2023/script.sh
+++ kyverno/cloud_native_aalborg_2023/script.sh
@@ -1,32 +1,30 @@
-pushd kyverno
-k create ns kyverno
+cd /Users/jinhong_brejnholt/Git/GitHub/demos/kyverno/cloud_native_aalborg_2023/kyverno
+# k create ns kyverno
 k apply -f secret-demo.yaml
 helm install -f values.yaml -f values-demo.yaml kyverno -n kyverno .
-popd
 
 # cosign
-k apply -f cosign/verify-image-signature.yaml
+cd /Users/jinhong_brejnholt/Git/GitHub/demos/kyverno/cloud_native_aalborg_2023/cosign
+k apply -f verify-image-signature-kv.yaml
 k run bad-busy --image=ghcr.io/jbrejnholt/cosign-demo/busybox-unsigned:1.34.1
-k run good-busy --image=ghcr.io/jbrejnholt/cosign-demo/busybox-signed:1.36.0
+k run good-busy --image=ghcr.io/jbrejnholt/cosign-demo/busybox-signed:1.36.0 --command sleep 30
 
 # sync secret
-pushd postgres-operator
+cd /Users/jinhong_brejnholt/Git/GitHub/demos/kyverno/cloud_native_aalborg_2023/postgres-operator
 helm install -f values.yaml postgres-operator .
-popd
 
 k create secret generic psql-backup-creds --from-literal=postgres-user=super-user --from-literal=password=abcdef12345 -n default
 k apply -f sync-secret/sync-postgres-cluster-secrets.yml
 k apply -f sync-secret/p-cluster.yaml
 
 # pdb validation
-pushd pdb-validation
+cd /Users/jinhong_brejnholt/Git/GitHub/demos/kyverno/cloud_native_aalborg_2023/pdb-validation
 k apply -f verify-pdb-configuration.yaml
 k apply -f pdbs.yaml
 k apply -f bad-busybox-min.yaml
 k apply -f good-busybox-max.yaml
-popd
 
 # bare pod clean up
-pushd cleanup
+cd /Users/jinhong_brejnholt/Git/GitHub/demos/kyverno/cloud_native_aalborg_2023/cleanup
 k apply -f cleanup-bare-pod.yaml
 k apply -f nginx-bare.yaml
