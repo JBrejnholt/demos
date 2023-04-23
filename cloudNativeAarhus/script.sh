ffbc03406e6c3e7c99562ef199b91e0ab4391ff3
jinhong.brejnholt <jinhong.brejnholt@velux.com>
2023-04-23 21:37:34 +0200

    feat: clean up pdb validations.

--- cloudNativeAarhus/script.sh
+++ cloudNativeAarhus/script.sh
@@ -4,13 +4,13 @@ k create ns kyverno
 helm install -f values.yaml -f values-demo.yaml kyverno -n kyverno .
 
 # pdb validation
-cd /Users/jinhong_brejnholt/Git/GitHub/cloudNativeAarhus/pdb-validation
+cd /Users/jinhong_brejnholt/Git/GitHub/demos/cloudNativeAarhus/pdb-validation
 k apply -f verify-pdb-configuration.yaml
 k apply -f pdbs.yaml
-k apply -f bad-busybox-min.yaml
-k apply -f good-busybox-max.yaml
+k apply -f bad-busybox.yaml
+k apply -f good-busybox.yaml
 
 # bare pod clean up
-cd /Users/jinhong_brejnholt/Git/cloudNativeAarhus/cleanup
+cd /Users/jinhong_brejnholt/Git/GitHub/demos/cloudNativeAarhus/cleanup
 k apply -f cleanup-bare-pod.yaml
 k apply -f nginx-bare.yaml
